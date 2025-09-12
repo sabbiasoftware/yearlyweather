53683d0ddf9426036461a7b6682d21174d406f52
Sabbia Soft <sabbiasoftware@gmail.com>
2025-09-12 23:06:33 +0200

    disable mock data

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ function generateStats() {
         locator.value.selectedLocation.lon,
         locator.value.selectedLocation.lat,
         dateSelector.value.selectedDateRanges,
-        true // useMockData
+        false // useMockData
     );
 }
 
