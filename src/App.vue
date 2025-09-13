45ad090567504ad018338a42495a37b78ead577b
Sabbia Soft <sabbiasoftware@gmail.com>
2025-09-13 21:37:49 +0200

    attempt to fix leaflet marker icon

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ function generateStats() {
         <br/>
 
         <Panel header="Yearly Weather" toggleable collapsed>
-            <p>Ever wonder what the weather was like on your birthdays? Whether it rained on Thanksgiving in the past 10 years? Or how about historical weather on first Saturdays of October, on the day of your fav running race? Seek no more. This little app is (hopefully) for you.</p>
+            <p>Ever wonder what the weather was like on your birthdays? Whether it rained on Thanksgiving in the past 10 years? Or how about historical weather on first Saturdays of October, on the day of your favorite running race? Seek no more. This little app is (hopefully) for you.</p>
             <p>Feel free to send any feedback to <a href="mailto://sabbiasoftware.gmail.com">sabbiasoftware@gmail.com</a>.</p>
             <p>Public web services used:</p>
             <ul>
