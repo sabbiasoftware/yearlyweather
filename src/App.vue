16c787e932863d542380ea8ccc6d26f44b229353
Sabbia Soft <sabbiasoftware@gmail.com>
2025-09-26 17:37:10 +0200

    favicon update

--- src/App.vue
+++ src/App.vue
@@ -53,6 +53,7 @@ onMounted(() => {
         <li>Geocoding: <a href="https://photon.komoot.io">photon.komoot.io</a></li>
         <li>Mapping: <a href="https://openstreetmap.org">openstreetmap.org</a></li>
         <li>Weather: <a href="https://open-meteo.com">open-meteo.com</a></li>
+        <li>Favicon: <a href="https://www.flaticon.com/free-icons/clouds-and-sun" title="clouds and sun icons">Clouds and sun icons created by riajulislam - Flaticon</a></li>
       </ul>
       <p><span class="strong">Note</span>: This page is NOT intended to forecast weather. It presents purely historical
         weather data.</p>
