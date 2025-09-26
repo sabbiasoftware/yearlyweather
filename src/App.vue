f70051dd81a85cc4cde40a086965cd40baa2f6e5
Sabbia <sabbiasoftware@gmail.com>
2025-09-26 15:52:05 +0200

    Favicon credit added

--- src/App.vue
+++ src/App.vue
@@ -48,11 +48,12 @@ onMounted(() => {
         years? Or how about historical weather on first Saturdays of October, on the day of your favorite running race?
         Seek no more. This little app is (hopefully) for you.</p>
       <p>Feel free to send any feedback to <a href="mailto://sabbiasoftware.gmail.com">sabbiasoftware@gmail.com</a>.</p>
-      <p>Public web services used:</p>
+      <p>Credits:</p>
       <ul>
         <li>Geocoding: <a href="https://photon.komoot.io">photon.komoot.io</a></li>
         <li>Mapping: <a href="https://openstreetmap.org">openstreetmap.org</a></li>
         <li>Weather: <a href="https://open-meteo.com">open-meteo.com</a></li>
+        <li>Favicon: <a href="https://www.flaticon.com/free-icons/clouds-and-sun" title="clouds and sun icons">Clouds and sun icons created by riajulislam - Flaticon</a></li>
       </ul>
       <p><span class="strong">Note</span>: This page is NOT intended to forecast weather. It presents purely historical
         weather data.</p>
